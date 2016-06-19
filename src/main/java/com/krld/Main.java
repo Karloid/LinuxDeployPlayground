439b11133aad0d8ffc731daa9cd8891c57e52519
h <h>
2016-06-19 14:19:08 +0300

    - hello world lanterna

--- src/main/java/com/krld/Main.java
+++ src/main/java/com/krld/Main.java
@@ -1,8 +1,42 @@
 package com.krld;
 
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.gui2.*;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("*** starting app ***");
+        Panel panel = new Panel();
+        panel.setLayoutManager(new GridLayout(2));
+
+        panel.addComponent(new Label("Forename"));
+        panel.addComponent(new TextBox());
+
+        panel.addComponent(new Label("Surname"));
+        panel.addComponent(new TextBox());
+
+        panel.addComponent(new EmptySpace(new TerminalSize(0, 0))); //empty space underneath labels
+        panel.addComponent(new Button("Submit"));
+
+        BasicWindow window = new BasicWindow();
+        window.setComponent(panel);
+        window.setTitle("Hello World window");
+
+
+        Terminal terminal = new DefaultTerminalFactory().createTerminal();
+        Screen screen = new TerminalScreen(terminal);
+        screen.startScreen();
+
+        MultiWindowTextGUI gui = new MultiWindowTextGUI(screen, new DefaultWindowManager(), new EmptySpace(TextColor.ANSI.BLUE));
+        gui.addWindowAndWait(window);
+
 
     }
 }
