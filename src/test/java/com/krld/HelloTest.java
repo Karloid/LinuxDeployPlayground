a058da0c251b551d551ff83cf3e2b3bf76d90d60
h <h>
2016-06-19 14:11:48 +0300

    - cleanup tests; - add lanterna

--- src/test/java/com/krld/HelloTest.java
+++ src/test/java/com/krld/HelloTest.java
@@ -14,25 +14,20 @@ public class HelloTest {
     @Before
     public void setUp() throws Exception {
         hello = new Hello();
-        System.out.println("setup");
     }
 
     @After
     public void tearDown() throws Exception {
-        System.out.println("tear down");
     }
 
     @Test
     public void getHello() throws Exception {
-        System.out.println("test hello class");
         String output = hello.getHello();
-        System.out.println(output);
         assertTrue(output.equalsIgnoreCase("hello"));
     }
 
     @Test
     public void calcSum() throws Exception {
-        System.out.println("test calc sum");
         assertEquals(hello.calcSum(10, 10), 20);
 
     }
